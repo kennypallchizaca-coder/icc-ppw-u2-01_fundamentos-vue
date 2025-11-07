54725719a9c5d49859a84e4c14d48502429840e9
Alex Guaman <kennypallchizaca@gmail.com>
2025-11-07 11:54:42 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/icc-ppw-u2-01_fundamentos-vue/', 
+  base: '/icc-ppw-u2-01_fundamentos-vue/',
 
   plugins: [
     vue(),
